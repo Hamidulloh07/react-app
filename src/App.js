098162b9e5025424ce09dc6009f945532f2b069c
Hamidulloh07 <hsiddiqxojayev2005@gmail.com>
2022-09-29 06:42:41 +1400

    first commit

---
+++ src/App.js
@@ -0,0 +1,310 @@
+import './App.css';
+
+
+function App() {
+  return (
+    <div>
+      <header>
+      <div className="container">
+        <div className="header">
+          <a className="logo-svg" href="ha">
+            <img src="https://raw.githubusercontent.com/Hamidulloh07/Pixer/dad0c3fcbe785822e19ea0e9060f00bfef7d335d/img/pixer-logo.svg" alt="logo" width="58" height="26"></img>
+          </a>
+          <nav className="nav">
+            <ul className="header-element">
+              <li className="header-item"><a className="header-links" href="nimadirbosh-sahifa">Bosh sahifa</a></li>
+              <li className="header-item">
+                <a className="header-links" href="./about.html">Xizmatlar</a>
+                <ul className="subb-element">
+                  <li className="subb-item"><a className="subb-link" href="nimadirweb">Vebsayt</a></li>
+                  <li className="subb-item"><a className="subb-link" href="nimadirtel">Telegram bot</a></li>
+                  <li className="subb-item"><a className="subb-link" href="nimadirsmm">SMM</a></li>
+                  <li className="subb-item"><a className="subb-link" href="nimadirgrafik">Grafik dizayn</a></li>
+                  <li className="subb-item"><a className="subb-link" href="nimadircrm">CRM tizim</a></li>
+                </ul>
+  
+              </li>
+              <li className="header-item"><a className="header-links" href="nimadirasdasd">Portfolio</a></li>
+              <li className="header-item"><a className="header-links" href="nimadirdasd">Jamoa</a></li>
+              <li className="header-item"><a className="header-links" href="nimadirbuyurtma">Blog</a></li>
+              <li className="header-item"><a className="header-links" href="nimadirfooter">Kontaktlar</a></li>
+            </ul>
+            <a className="headerr-link" href="tel:+998909213711">+998 90 921 37 11</a>
+  
+          </nav>
+        </div>
+      </div>
+    </header>
+      <main>
+       <section>
+          <div class="container">
+            <div id="bosh-sahifa" class="hero">
+              <div class="hero-2">
+                <h1 class="hero-tittle">
+                  Biznesingizni keyingi bosqichga
+                  olib chiqing
+                </h1>
+                <p class="hero-text">
+                  Nullam laoreet nec turpis et ultrices. Duis sit amet quam arcu. Nam facilisis lacinia ex, eget
+                  sollicitudin massa pellentesque in. Vivamus mattis eros at sem pulvinar
+                </p>
+                <a href="./about.html" class="hero-link">Xizmatlar bilan tanishish</a>
+    
+              </div>
+              <iframe class="iframe" width="458" height="258" src="https://www.youtube.com/embed/6_pru8U2RmM"
+                title="YouTube video player"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen></iframe>
+            </div>
+          </div>
+        </section>
+
+        <section id="xizmatlar">
+      <div class="container">
+        <div class="intro">
+          <h2 class="intro-title">
+            Xizmatlar
+          </h2>
+          <p class="intro-text">
+            Nullam laoreet nec turpis et ultrices. Duis sit amet quam arcu. Nam facilisis lacinia ex, eget sollicitudin
+            massa pellentesque in. Vivamus mattis eros at sem pulvinar
+          </p>
+          <ul class="intro-element">
+            <li id="web" class="intro-item">
+              <a class="item-link" href="nimadir">
+                <img class="sub-img" src="	https://github.com/Hamidulloh07/Pixer/blob/main/img/crm.png?raw=true" alt="prices" width="202" height="130"></img>
+                <h3 class="intro-sub-title">
+                  Vebsayt tuzish
+                </h3>
+                <p class="intro-sub-text">
+                  lorem ipsum
+                </p>
+              </a>
+            </li>
+            <li id="tel" class="intro-item">
+              <a class="item-link" href="nimadir">
+                <img class="sub-img" src="https://github.com/Hamidulloh07/Pixer/blob/main/img/firs-li.png?raw=true" alt="prices" width="222" height="130"></img>
+                <h3 class="intro-sub-title">
+                  Telegram bot
+                </h3>
+                <p class="intro-sub-text">
+                  lorem ipsum
+                </p>
+              </a>
+            </li>
+
+            <li id="smm" class="intro-item">
+              <a class="item-link" href="nimadir">
+                <img class="sub-img" src="https://github.com/Hamidulloh07/Pixer/blob/main/img/game.png?raw=true" alt="prices" width="176" height="130"></img>
+                <h3 class="intro-sub-title">
+                  SMM
+                </h3>
+                <p class="intro-sub-text">
+                  lorem ipsum
+                </p>
+              </a>
+            </li>
+
+            <li id="grafik" class="intro-item">
+              <a class="item-link" href="nimadir">
+                <img class="sub-img" src="https://github.com/Hamidulloh07/Pixer/blob/main/img/phone.png?raw=true
+" alt="prices" width="178" height="130"></img>
+                <h3 class="intro-sub-title">
+                  Grafik dizayn
+                </h3>
+                <p class="intro-sub-text">
+                  lorem ipsum
+                </p>
+              </a>
+            </li>
+
+            <li id="crm" class="intro-item">
+              <a class="item-link" href="nimadir">
+                <img class="sub-img" src="https://github.com/Hamidulloh07/Pixer/blob/main/img/step.png?raw=true" alt="prices" width="169" height="130"></img>
+                <h3 class="intro-sub-title">
+                  CRM tizimlar
+                </h3>
+                <p class="intro-sub-text">
+                  lorem ipsum
+                </p>
+              </a>
+            </li>
+
+          </ul>
+          <span class="span-intro">
+            <a class="span-link" href="nimadir">Buyurtma berish</a>
+            <a class="span-link-2" href="./about.html">Xizmatlar sahifasiga o’tish</a>
+          </span>
+        </div>
+      </div>
+    </section>
+        
+    <section>
+      <div class="container">
+        <div class="price">
+          <div class="title-div">
+            <h2 class="price-title">
+              Biz qanday ishlaymiz?
+            </h2>
+            <p class="price-text">
+              Nullam laoreet nec turpis et ultrices. Duis sit amet quam arcu. Nam facilisis lacinia ex, eget
+              sollicitudin massa pellentesque in. Vivamus mattis eros at sem pulvinar
+            </p>
+
+          </div>
+          <ul class="price-element">
+            <li class="price-item">
+              <img src="https://github.com/Hamidulloh07/Pixer/blob/main/img/look.png?raw=true" alt="look-img" width="300" height="278"></img>
+              <div class="sub-price">
+                <h3 class="sub-pruce-title">
+                  Talablarni aniqlab chiqamiz
+                </h3>
+                <p class="sub-pruce-text">
+                  Nullam laoreet nec turpis et ultrices. Duis sit amet quam arcu. Nam facilisis
+                </p>
+              </div>
+            </li>
+
+            <li class="price-item">
+              <img src="https://github.com/Hamidulloh07/Pixer/blob/main/img/look-4.png?raw=true" alt="look-img" width="300" height="224"></img>
+              <div class="sub-price">
+                <h3 class="sub-pruce-title">
+                  Bir necha yechimlarni taklif qilamiz
+                </h3>
+                <p class="sub-pruce-text">
+                  nec turpis et ultrices. Duis sit amet quam arcu. Nam facilisis
+                </p>
+              </div>
+            </li>
+
+            <li class="price-item">
+              <img src="	https://github.com/Hamidulloh07/Pixer/blob/main/img/look-3.png?raw=true" alt="look-img" width="300" height="258"></img>
+              <div class="sub-price">
+                <h3 class="sub-pruce-title">
+                  Loyiha uchun vaqt belgilaymiz
+
+                </h3>
+                <p class="sub-pruce-text">
+                  nec turpis et ultrices. Duis sit amet quam arcu. Nam facilisis
+                </p>
+              </div>
+            </li>
+
+            <li class="price-item">
+              <img src="	https://github.com/Hamidulloh07/Pixer/blob/main/img/look-2.png?raw=true" alt="look-img" width="300" height="171"></img>
+              <div class="sub-price">
+                <h3 class="sub-pruce-title">
+                  A’lo sifat bilan bajarib topshiramiz
+                </h3>
+                <p class="sub-pruce-text">
+                  nec turpis et ultrices. Duis sit amet quam arcu. Nam facilisis
+                </p>
+              </div>
+            </li>
+
+            <li class="price-item">
+              <img src="	https://github.com/Hamidulloh07/Pixer/blob/main/img/telegram.png?raw=true" alt="look-img" width="300" height="232"></img>
+              <div class="sub-price">
+                <h3 class="sub-pruce-title">
+                  Qo’llab-quvvatlab boramiz
+                </h3>
+                <p class="sub-pruce-text">
+                  nec turpis et ultrices. Duis sit amet quam arcu. Nam facilisis </p>
+              </div>
+            </li>
+
+          </ul>
+          <a href="sadasd" class="price-linkk">Buyurtma berish</a>
+        </div>
+      </div>
+    </section>
+
+    <section id="buyurtma" class="section-form">
+      <div class="container">
+        <div class="inner">
+          <div class="inner-title-div">
+            <h2 class="inner-title">
+              Buyurtma berish
+            </h2>
+            <p class="inner-text">
+              Nullam laoreet nec turpis et ultrices. Duis sit amet quam arcu. Nam facilisis lacinia ex, eget
+              sollicitudin massa pellentesque in. Vivamus mattis eros at sem pulvinar
+            </p>
+          </div>
+
+          <form class="form" autocomplete="off" action="https://echo.htmlacademy.ru/" >
+
+            <label class="label">
+              <input class="input" type="text" name="user-name" placeholder="Ismingiz" required></input>
+            </label>
+
+            <label class="label">
+              <input class="input" type="tel" name="user-phone" placeholder="Telefon raqamingiz" required></input>
+            </label>
+            <select name="Xizmat" class="form-input" id="Xizmat">
+              <option value="select" selected disabled hidden>Xizmat turi</option>
+              <optgroup label="Xizmatlar">
+              <option value="Sayt tuzish">Sayt tuzish</option>
+              <option value="Telegram bot tuzish">Telegram bot tuzish</option>
+              <option value="SMM">SMM</option>
+              <option value="Grafik dizayn">Grafik dizayn</option>
+              <option value="CRM tizimlar">CRM tizimlar</option>
+              </optgroup>
+
+
+            </select>
+            <button class="button" type="submit">Buyurtma berish</button>
+
+
+          </form>
+
+
+        </div>
+      </div>
+    </section>
+      </main>
+
+
+      <footer id="footer">
+    <div class="container">
+      <div class="footer">
+        <div class="footer-div">
+          <p class="div-text">
+            Copyright 2020
+          </p>
+
+          <p class="div-text-2">
+            Nullam laoreet nec turpis et ultrices. Duis sit amet quam arcu. Nam facilisis lacinia ex, eget sollicitudin
+            massa pellentesque in. Vivamus mattis eros at sem pulvinar tincidunt.
+          </p>
+
+        </div>
+
+        <ul class="footer-element">
+          <li class="footer-item"><p class="itim-text">Sayt sahifalari</p></li>
+          <li class="footer-item"><a href="sss" class="element-link">Bosh sahifa</a></li>
+          <li class="footer-item"><a href="sss" class="element-link">Xizmatlar</a></li>
+          <li class="footer-item"><a href="sss" class="element-link">Portfolio</a></li>
+          <li class="footer-item"><a href="sss" class="element-link">Jamoa</a></li>
+          <li class="footer-item"><a href="sss" class="element-link">Blog</a></li>
+          <li class="footer-item"><a href="sss" class="element-link">Kontaktlar</a></li>
+        </ul>
+
+        <ul class="footer-element2">
+          <li class="footer-itm-2"><p class="itim-text">Biz internetda</p></li>
+          <li class="footer-itm-2"><a href="https://telegram.com" class="footer-link-2"><img className='margin-right' src="	https://raw.githubusercontent.com/Hamidulloh07/Pix…2e19ea0e9060f00bfef7d335d/img/telegram-vector.svg" alt="telegram" class="link-img" width="22" height="23"></img>Telegram</a></li>
+          <li class="footer-itm-2"><a href="https://facebook.com" class="footer-link-2"><img className='margin-right' src="https://raw.githubusercontent.com/Hamidulloh07/Pix…2e19ea0e9060f00bfef7d335d/img/facebook-vector.svg" alt="facebook" class="link-img" width="22" height="23"></img>Facebook</a></li>
+          <li class="footer-itm-2"><a href="https://instagram.com" class="footer-link-2"><img className='margin-right' src="	https://raw.githubusercontent.com/Hamidulloh07/Pix…e19ea0e9060f00bfef7d335d/img/instagram-vector.svg" alt="instagram" class="link-img" width="22" height="23"></img>Instagram</a></li>
+        </ul>
+
+
+      </div>
+    </div>
+  </footer>
+    </div>
+
+    
+  );
+}
+
+export default App;
